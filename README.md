4108bae56931732ee4ae59a44f19f5aca5449771
Diego de Jesus Ochoa Garcia <ddjochoa.20@gmail.com>
2020-10-04 22:44:30 -0600

    Correcciones

--- README.md
+++ README.md
@@ -8,6 +8,8 @@ _Tener WampServer instalado les dejo el link aqui:_
 * [Visual Studio 2012: VC 11 vcredist_x64 / 86.exe](https://www.microsoft.com/en-us/download/details.aspx?id=30679) - Instala dependencias funcionales para Wampserver
 (Instala segun tu arquitectura 32 o 64 bits).
 * [Wampserver](https://www.wampserver.com/en/) - en la sección de descargas.
+
+_hacer el git clone en la carpeta wwww donde esta la instalacion wampserver_
 ## Autores ✒️
 * **Diego Ochoa** - *Project Manager* 
 * **René Martínez** - *Project Manager* 
--- arreglos.php
+++ arreglos.php
@@ -5,13 +5,17 @@
     $primer_arreglo[0] = 'Hola';
     $primer_arreglo[1] = 'Mundo';
     echo 'Mensaje de bienvenida: <b>'.$primer_arreglo[0].' '.$primer_arreglo[1].'</b><br>';
+    echo '<br><br>';
     print_r($primer_arreglo);
     echo '<br><br>';
     //podemos modificar el nombre de los indices(clave) de un array
+    //Este es un array asociativo 
     $array = [
         "foo" => "bar",
         "bar" => "foo",
     ];
+    //podemos imprimir un dato del arreglo por su clave 
+    echo $array['foo']."<br><br>";
     print_r($array);
    
     
